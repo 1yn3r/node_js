083e708bc9e9774c31cfdb491b118f8bd9c18dce
1yn3r <n21dcat007@student.ptithcm.edu.vn>
2024-03-17 21:32:22 +0700

    up

--- src/index.js
+++ src/index.js
@@ -9,10 +9,20 @@ const port = 3000
 
 app.use(express.static(path.join(__dirname,'public')))
 
-app.use(morgan('combined'))
+app.use(express.urlencoded({ 
+  extended: true
+}))
+app.use(express.json())
+
+// HTTP logger
+// app.use(morgan('combined'))
+
+// Template engine
 app.engine('hbs', engine(
   {extname: '.hbs'}
 ))
+
+
 app.set('view engine', 'hbs')
 app.set('views', path.join(__dirname, 'resources/views'))
 
@@ -29,7 +39,8 @@ app.get('/search', (req, res) => {
 })
 
 app.post('/search', (req, res) => {
-  res.send('A')
+  console.log(req.body)
+  res.send('')
 })
 
 app.listen(port, () => {
