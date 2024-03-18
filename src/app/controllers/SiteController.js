664f2474184058e3f7e6dcdbb98681ba317baa8e
1yn3r <hc223390@gmail.com>
2024-03-18 16:01:19 +0700

    khoa hoac

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,17 +1,21 @@
 const Course = require("../models/Course");
+const { mutipleMongooseToObject } = require("../../util/mongoose");
 
 class SiteController {
-  // [GET] /news
-  index(req, res) {
+  // [GET] /
+  index(req, res, next) {
     
-    Course.find().then(courses => {
-      res.json(courses);
-    })
-    .catch(err => {
-      res.status(400).json({ error: "ERROR!!!" });
-    // res.render("home");
-    });
+    Course.find({})
+      .then(courses => {
+        res.render("home",{
+          courses: mutipleMongooseToObject(courses)
+        })
+      })
+
+      
+      .catch(next)
   }
+
 
   // [GET] /search
   search(req, res) {
